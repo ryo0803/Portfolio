0c8e600ad7747a22be59adc4ed22aab96f71c5b5
nishizaki <rn.383.b2fz@gmail.com>
2021-06-21 03:40:33 +0900

    fix update.html

--- labo/forms.py
+++ labo/forms.py
@@ -7,3 +7,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = '__all__'
+
+        widgets = {
+            'comment': forms.Textarea(attrs={'rows': 10, 'cols': 100}),
+        }
